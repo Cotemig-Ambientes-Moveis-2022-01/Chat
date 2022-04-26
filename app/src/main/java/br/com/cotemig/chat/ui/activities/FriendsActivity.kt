d441581a4d63032c4773452bc635078cf2d32234
Dirceu Belém <dirceu@fourtime.com>
2022-04-26 13:25:58 -0300

    Organizando

---
+++ app/src/main/java/br/com/cotemig/chat/ui/activities/FriendsActivity.kt
@@ -0,0 +1,12 @@
+package br.com.cotemig.chat.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.cotemig.chat.R
+
+class FriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+    }
+}
